3250fc0a9921977201c615c88b6e201052e9543c
ahmedhamzarana <ahmedhamzarana000@gmail.com>
2025-11-24 13:29:26 +0500

    make dynamic chat screen

--- lib/Dashboard/chat_screen.dart
+++ lib/Dashboard/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/Dashboard/utills/chat_box_view.dart';
 import 'package:whatsapp/Dashboard/utills/custom_widget.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -9,6 +10,16 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  List<Map<String, String>> mycontact = [
+    {
+      "imgurl":
+          "https://media.licdn.com/dms/image/v2/D4E03AQG2UjPoyRyQJw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1691323841878?e=2147483647&v=beta&t=a5yn15e7K7xzlRmiptWgO36VgQqvf_RIN1RqjOCJ0x8",
+      "name": "Hamza",
+      "message": "Kaha hu ?",
+      "time": "yesterday",
+      "count": "12",
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,11 +45,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   },
                   icon: Icon(Icons.camera_alt_outlined),
                 ),
-                IconButton(
-                  onPressed: () {
-                  },
-                  icon: Icon(Icons.more_vert),
-                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
               ],
             ),
           ),
@@ -47,23 +54,26 @@ class _ChatScreenState extends State<ChatScreen> {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
             child: TextField(
               decoration: InputDecoration(
-                label: Text(
-                  "Ask Meta Ai or Search",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 172, 173, 172),
-                    fontSize: 15,
-                  ),
-                ),
+                labelText: "Ask Meta Ai or Search",
+                labelStyle: TextStyle(color: Colors.grey, fontSize: 15),
                 prefixIcon: Icon(Icons.search),
               ),
             ),
           ),
 
-          
+          ListView.builder(
+            itemCount: mycontact.length,
+            itemBuilder: (context, index) => ChatBoxView(
+              imgurl: mycontact[0]["imgurl"].toString(),
+              name: mycontact[0]["name"].toString(),
+              message: mycontact[0]["message"].toString(),
+              time: mycontact[0]["time"].toString(),
+              count: mycontact[0]["count"].toString(),
+            ),
+          ),
         ],
       ),
     );
--- lib/Dashboard/utills/chat_box_view.dart
+++ lib/Dashboard/utills/chat_box_view.dart
@@ -1,7 +1,20 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:whatsapp/Dashboard/utills/custom_widget.dart';
 
 class ChatBoxView extends StatefulWidget {
-  const ChatBoxView({super.key});
+  final String imgurl;
+  final String name;
+  final String message;
+  final String time;
+  final String count;
+
+  const ChatBoxView({super.key, 
+    required this.imgurl,
+    required this.name,
+    required this.message,
+    required this.time,
+    required this.count,
+  });
 
   @override
   State<ChatBoxView> createState() => _ChatBoxViewState();
@@ -10,6 +23,64 @@ class ChatBoxView extends StatefulWidget {
 class _ChatBoxViewState extends State<ChatBoxView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+      child: Row(
+        children: [
+          CircleAvatar(backgroundImage: NetworkImage(widget.imgurl)),
+          SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.name,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                widget.message,
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              ),
+            ],
+          ),
+          Spacer(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                widget.time,
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                ),
+              ),
+              SizedBox(height: 3),
+              Container(
+                height: 20,
+                width: 20,
+                decoration: BoxDecoration(
+                  color: AppColors.primarycolor,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Center(
+                  child: Text(
+                    widget.count,
+                    style: TextStyle(
+                      color: AppColors.backgroundcolor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 8,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
